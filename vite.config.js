fc7ef515528ae57748dda9aa6ce63a38a93de2a6
Abdelilah07 <mabroukabdelilah07@gmail.com>
2024-12-02 17:13:05 +0100

    deployement

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    build: {
+      outDir: 'docs',
+    },
+  }
+
+  if (command === 'build') {
+    config.base = '/business-model-canvas'
+  }
+
+  return config
 })
